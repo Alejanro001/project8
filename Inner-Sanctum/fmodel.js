c618ae0fe79bc4b81b341aa034d9a097b93cfa23
BasiliskOps <noro.nero@gmail.com>
2021-03-24 18:30:37 -0500

    full send on the backend

--- Inner-Sanctum/fmodel.js
+++ Inner-Sanctum/fmodel.js
@@ -1,9 +1,9 @@
 let mongoose = require('mongoose')
-let Schema = mongoose.Schema
+
 
 const fighterSchema = new mongoose.Schema({
     name: String,
-    attacks: [String],
+    attacks: String,
     description: String,
     origin: String
 })
