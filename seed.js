44c0b9f464169b9d7e4276736c72e6c4208234b7
BasiliskOps <noro.nero@gmail.com>
2021-03-25 10:49:37 -0500

    heroku login broke seeded data to mongo

--- seed.js
+++ seed.js
@@ -3,14 +3,17 @@ let mongoose = require('mongoose')
 let fighterModel = require('./Inner-Sanctum/fmodel')
 let trackModel = require('./soundmodel')
 let websiteData = require('./fighter.json')
-fighterModel.deleteMany({}).then(
+fighterModel.deleteMany({}).then( () => {
+    const allFighters = websiteData.fighters
+    console.log(allFighters)
+
     trackModel.create(websiteData).then((fighters) => {
         console.log(fighters.length, 'Application data established')
     })
-)
+})
 
 trackModel.deleteMany({}).then( () => {
-    const allTracks = websiteData[0].Tracks
+    const allTracks = websiteData.tracks
     console.log(allTracks)
     trackModel.create(allTracks).then((fighters) => {
         console.log(fighters.length, 'The tracks have been established')
